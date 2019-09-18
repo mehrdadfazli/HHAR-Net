aeb5bf8177a6f39fd7ccd7ccc6212818ed7c8163
mehrdadfazli <43502750+mehrdadfazli@users.noreply.github.com>
2019-09-18 00:13:47 -0400

    Add files via upload

--- Hierarchical Activity Recognition.py
+++ Hierarchical Activity Recognition.py
@@ -12,8 +12,16 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
+from keras.models import Sequential
+from keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import glob
 import os
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score
 
 # - data passed should have all the labels (parent and child level) ready
 
@@ -98,7 +106,7 @@ def data_cleaner(feature_set_range, parent_labels, child_labels, dataset):
     
     # Cutting out the features and labels from the entire dataset
     features = dataset.iloc[:,feature_set_range]
-    labels = dataset[parent_labels]    
+    labels = dataset[parent_labels]
     
     # Consider eliminating the for loops
     for i in range(num_parents):
@@ -147,6 +155,7 @@ def data_cleaner(feature_set_range, parent_labels, child_labels, dataset):
     
     # Collecting X and y for the training phase of the classifiers
     X_parent = raw_data.iloc[:,feature_set_range]
+    X_parent = preprocessing.scale(X_parent, axis=1)
     y_parent = raw_data['Parent_label_index']
     
     X_child = []
@@ -154,6 +163,7 @@ def data_cleaner(feature_set_range, parent_labels, child_labels, dataset):
     for i in range(num_parents):
         dataset_ith_parent = raw_data[raw_data['Parent_label_index'] == i]
         X_child.append(dataset_ith_parent.iloc[:,feature_set_range])
+        X_child[i] = preprocessing.scale(X_child[i], axis=1)
         y_child.append(dataset_ith_parent['Child_label_index'])
     
     return(X_parent, y_parent, X_child, y_child)
@@ -174,7 +184,6 @@ if __name__ == '__main__':
     
     feature_set_range = range(1,84)
     
-    
     ##########################################################################
     #-----------------| Stationary vs NonStationary |------------------------#
     ##########################################################################
@@ -192,7 +201,7 @@ if __name__ == '__main__':
             response = pd.concat([response,dataset[child_labels[i][j]]], axis=1)
 
     data_to_model = pd.concat([feat,response],axis=1)
-    data_to_model = dataset.dropna()
+    data_to_model = data_to_model.dropna()
     
     data_to_model['Stationary'] = np.logical_or(data_to_model['label:OR_standing'],data_to_model['label:SITTING'])
     data_to_model['Stationary'] = np.logical_or(data_to_model['Stationary'],data_to_model['label:LYING_DOWN'])*1
@@ -207,10 +216,39 @@ if __name__ == '__main__':
     #-------------------------| Indoor vs Outdoor |---------------------------#
     ###########################################################################
     
-    parent_labels = ['label:OR_indoors','label:OR_outside']
+#    parent_labels = ['label:OR_indoors','label:OR_outside']
+#    
+#    child_labels = [['label:IN_A_MEETING','label:IN_CLASS','label:AT_HOME'],\
+#                    ['label:FIX_running','label:FIX_walking','label:BICYCLING']]
+#    
+#    feat = dataset.iloc[:,feature_set_range]
+#    response = pd.DataFrame()
+#    for i in range(len(parent_labels)):
+#        for j in range(len(child_labels[i])):
+#            response = pd.concat([response,dataset[child_labels[i][j]]], axis=1)
+#
+#    dataset = pd.concat([feat,response],axis=1)
+#    dataset = dataset.dropna()
+#    
+#    dataset['Stationary'] = np.logical_or(dataset['label:OR_standing'],dataset['label:SITTING'])
+#    dataset['Stationary'] = np.logical_or(dataset['Stationary'],dataset['label:LYING_DOWN'])*1
+#    
+#    dataset['NonStationary'] = np.logical_or(dataset['label:FIX_running'],dataset['label:FIX_walking'])
+#    dataset['NonStationary'] = np.logical_or(dataset['NonStationary'],dataset['label:BICYCLING'])*1
+#       
+#    feature_set_range = range(83)
+#    X_parent, y_parent, X_child, y_child = data_cleaner(feature_set_range, parent_labels, child_labels, dataset)
     
-    child_labels = [['label:IN_A_MEETING','label:IN_CLASS','label:AT_HOME'],\
-                    ['label:FIX_running','label:FIX_walking','label:BICYCLING']]
+    
+    ###########################################################################
+    #----------------------------| Phone Position |---------------------------#
+    ###########################################################################
+    
+    parent_labels = ['label:PHONE_IN_BAG','label:PHONE_IN_HAND','label:PHONE_IN_POCKET',\
+                     'label:PHONE_ON_TABLE']
+    
+    child_labels = [['label:PHONE_IN_BAG'],['label:PHONE_IN_HAND'],['label:PHONE_IN_POCKET'],\
+                    ['label:PHONE_ON_TABLE']]
     
     feat = dataset.iloc[:,feature_set_range]
     response = pd.DataFrame()
@@ -221,14 +259,46 @@ if __name__ == '__main__':
     dataset = pd.concat([feat,response],axis=1)
     dataset = dataset.dropna()
     
-    dataset['Stationary'] = np.logical_or(dataset['label:OR_standing'],dataset['label:SITTING'])
-    dataset['Stationary'] = np.logical_or(dataset['Stationary'],dataset['label:LYING_DOWN'])*1
-    
-    dataset['NonStationary'] = np.logical_or(dataset['label:FIX_running'],dataset['label:FIX_walking'])
-    dataset['NonStationary'] = np.logical_or(dataset['NonStationary'],dataset['label:BICYCLING'])*1
+#    dataset['Stationary'] = np.logical_or(dataset['label:OR_standing'],dataset['label:SITTING'])
+#    dataset['Stationary'] = np.logical_or(dataset['Stationary'],dataset['label:LYING_DOWN'])*1
+#    
+#    dataset['NonStationary'] = np.logical_or(dataset['label:FIX_running'],dataset['label:FIX_walking'])
+#    dataset['NonStationary'] = np.logical_or(dataset['NonStationary'],dataset['label:BICYCLING'])*1
        
     feature_set_range = range(83)
     X_parent, y_parent, X_child, y_child = data_cleaner(feature_set_range, parent_labels, child_labels, dataset)
+
+    
+    ###########################################################################
+    #------------------------------| DNN |------------------------------------#
+    ###########################################################################
+    
+    X_parent_train, X_parent_test, y_parent_train, y_parent_test = \
+    train_test_split(X_parent, y_parent, test_size=0.3)
+    
+    clf_parent = Sequential()
+    clf_parent.add(Dense(1024, input_dim=len(feature_set_range), activation='relu'))
+    clf_parent.add(Dense(512, activation='relu'))
+    clf_parent.add(Dense(64, activation='relu'))
+    clf_parent.add(Dense(1, activation='sigmoid'))
+    
+    clf_parent.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    clf_parent.fit(X_parent_train, y_parent_train, batch_size=100, \
+                   validation_data=(X_parent_test, y_parent_test.values), \
+                   epochs=10, class_weight='balanced')
+    
+    y_parent_pred = clf_parent.predict(X_parent_test)
+    
+    #confusion_matrix = confusion_matrix(y_parent_test.values, y_parent_pred.round)
+    
+    df_cm = pd.DataFrame(confusion_matrix, range(2),range(2))
+#    plt.figure(figsize = (10,7))
+    sn.set(font_scale=1.4)#for label size
+    sn.heatmap(df_cm, annot=True,annot_kws={"size": 16})# font size
+    f1_score(y_parent_test.values, y_parent_pred.round(), average='macro')
+    
+
 
 
 
