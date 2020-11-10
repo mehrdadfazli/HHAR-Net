2b9b62ddde5bd6debaff084d2537eeb7a1307a8a
Kamran  Kowsari <kk7nc@virginia.edu>
2020-11-09 16:16:15 -0800

    Update and rename Inputs_HDLAct.py to HHAR-net/example/Inputs_HDLAct.py

--- HHAR-net/example/Inputs_HDLAct.py
+++ HHAR-net/example/Inputs_HDLAct.py
@@ -5,8 +5,8 @@ Created on Wed Oct  2 12:57:42 2019
 @author: 14342
 """
 
-data_dir = "C:\\Mehrdad\\DARPA-WASH\\datasets\\"
-cvdir = "C:\\Mehrdad\\DARPA-WASH\\cv5Folds\\cv_5_folds\\"
+data_dir = "..\\datasets\\"
+cvdir = "..\\cv5Folds\\cv_5_folds\\"
 
 sensors_to_use = ['Acc', 'Gyro', 'Mag']#, 'Compass', 'Loc', 'PS']
 
